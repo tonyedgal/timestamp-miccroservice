fa075c29a0606a125e76f4f87728e0fb2b806794
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-09 13:51:36 +0100

    feat: Completed the building of the timestamp microservice API

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 require("dotenv").config();
-// server.js
-// where your node app starts
 
-// init project
 var express = require("express");
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require("cors");
+const { response } = require("express");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 const SERVER_PORT = process.env.PORT || 8000;
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -26,7 +19,41 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+let timeObj = {};
+app.get("/api/timestamp", (req, res) => {
+  // timeObj["unix"] = new Date().getTime();
+  // timeObj["utc"] = new Date().toUTCString();
+  timeObj = { unix: new Date().getTime(), utc: new Date().toUTCString() };
+
+  res.json(timeObj);
+});
+
+app.get("/api/timestamp/:input", (req, res) => {
+  let input = req.params.input; //set input to e equals to req params
+
+  if (input.includes("-")) {
+    // check if input is a valid date string
+    timeObj = {
+      unix: new Date(input).getTime(),
+      utc: new Date(input).toUTCString(),
+    };
+  } else {
+    input = parseInt(input); // parse string to number
+
+    timeObj = {
+      unix: new Date(input).getTime(),
+      utc: new Date(input).toUTCString(),
+    };
+  }
+
+  // if (!timeObj["unix"] || !timeObj["utc"]) {
+  //   // use bracket notation check date validity
+  //   res.json({ error: "Invalid Date" });
+  // }
+  res.json(timeObj);
+});
+
 // listen for requests :)
-var listener = app.listen(SERVER_PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.listen(SERVER_PORT, () => {
+  console.log(`Your app is listening on port ${SERVER_PORT}`);
 });
